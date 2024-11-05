f734dc4bf393bb78de9b08a7e8235678c00ee4ee
Narayani KK <narayanikkhatavkar@gmail.com>
2024-11-04 23:09:47 -0400

    ecosystem.config.js

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,18 +1,15 @@
 module.exports = {
     apps: [
       {
-        name: 'app',
-        script: './app.js',
-        instances: 1,
-        autorestart: true,
-        watch: false,
+        name: "app",
+        script: "./app.js",
         env: {
-          NODE_ENV: 'development',
-          MONGODB_URI: 'mongodb+srv://narayanikkhatavkar:7ioqGiMC1jWisa45@shopifycluster.nmohw.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=ShopifyCluster'
+          NODE_ENV: "development",
+          MONGODB_URI: "mongodb+srv://narayanikkhatavkar:7ioqGiMC1jWisa45@shopifycluster.nmohw.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=ShopifyCluster"
         },
         env_production: {
-          NODE_ENV: 'production',
-          MONGODB_URI: 'your_production_mongodb_uri' // Set this to your production URI if applicable
+          NODE_ENV: "production",
+          MONGODB_URI: "mongodb+srv://<username>:<password>@shopifycluster.nmohw.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=ShopifyCluster"
         }
       }
     ]
