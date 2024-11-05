0942665d41d629ea5ae87d2c18624f8f5491f159
Narayani KK <narayanikkhatavkar@gmail.com>
2024-11-04 22:50:37 -0400

    Added ecosystem.config.js file

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,11 +1,18 @@
 module.exports = {
     apps: [
       {
-        name: "app",
-        script: "./app.js",
+        name: 'app',
+        script: './app.js',
+        instances: 1,
+        autorestart: true,
+        watch: false,
         env: {
-          MONGODB_URI: "mongodb+srv://narayanikkhatavkar:7ioqGiMC1jWisa45@shopifycluster.nmohw.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=ShopifyCluster",
-          NODE_ENV: "production"
+          NODE_ENV: 'development',
+          MONGODB_URI: 'mongodb+srv://narayanikkhatavkar:7ioqGiMC1jWisa45@shopifycluster.nmohw.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=ShopifyCluster'
+        },
+        env_production: {
+          NODE_ENV: 'production',
+          MONGODB_URI: 'your_production_mongodb_uri' // Set this to your production URI if applicable
         }
       }
     ]
